807b2713811476da078543be6c7057c4114d50dc
Ashwin Easo <ashwineaso@gmail.com>
2022-05-15 11:44:01 +0200

    Modified the models to represent the dynamodb tables

--- whoopbot/models.py
+++ whoopbot/models.py
@@ -1,42 +1,9 @@
-import uuid
+import os
+from datetime import datetime
 
-from sqlalchemy import (
-    CHAR,
-    Column,
-    DateTime,
-    ForeignKey,
-    String,
-    TypeDecorator
-)
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.functions import current_timestamp
+from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
 
 from whoopbot.db import Base
-
-
-class GUID(TypeDecorator):
-    """
-    Platform independent GUID type uses CHAR(32) stored as string hex values.
-    """
-
-    impl = CHAR
-
-    def load_dialect_impl(self, dialect):
-        return dialect.type_descriptor(CHAR(32))
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        if not isinstance(value, uuid.UUID):
-            return "%.32x" % uuid.UUID(value).int
-        return "%.32x" % value.int
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        if not isinstance(value, uuid.UUID):
-            value = uuid.UUID(value)
-        return value
 
 
 class OrgResource(Base):
@@ -44,15 +11,15 @@ class OrgResource(Base):
     Table of resources for an organization.
     """
 
-    __tablename__ = 'org_resources'
-
-    id = Column(GUID(), primary_key=True,
-                default=lambda: str(uuid.uuid4()), index=True)
-    resource_name = Column(String(255), nullable=False)
-    environment = Column(String(255), nullable=True)
+    resource_name = UnicodeAttribute(hash_key=True)
+    environment = UnicodeAttribute(range_key=True)
 
     def to_string(self):
         return f"{self.resource_name} - {self.environment}"
+
+    class Meta:
+        table_name = 'org_resources'
+        region = os.getenv('AWS_REGION')
 
 
 class LockedResource(Base):
@@ -60,15 +27,13 @@ class LockedResource(Base):
     Table of locked resources for an organization.
     """
 
-    __tablename__ = 'locked_resources'
+    resource_name = UnicodeAttribute(hash_key=True)
+    environment = UnicodeAttribute(range_key=True)
+    owner_id = UnicodeAttribute()
+    locked_at = UTCDateTimeAttribute(default=datetime.utcnow())
+    expires_at = UTCDateTimeAttribute()
 
-    id = Column(GUID(), primary_key=True,
-                default=lambda: str(uuid.uuid4()), index=True)
-    org_resource_id = Column(GUID(), ForeignKey('org_resources.id'),
-                             nullable=False, index=True)
-    owner_id = Column(String(255), nullable=False)
-    locked_at = Column(DateTime, nullable=False, default=current_timestamp())
-    expires_at = Column(DateTime, nullable=False)
-
-    org_resource = relationship('OrgResource', backref='locked_resources')
+    class Meta:
+        table_name = 'locked_resources'
+        region = os.getenv('AWS_REGION')
 
